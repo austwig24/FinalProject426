abe0441e3fcf686c6cc807ea4165ef72fdc2797f
austwig24 <austinludwig24@gmail.com>
2018-12-11 20:50:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-var root_url = "http://comp426.cs.unc.edu:3001/";
+var root_url = 'http://comp426.cs.unc.edu:3001';
 var sel_airline = '';
+var sel_id = 0;
 
 
 function build_homepage() {
@@ -19,10 +20,39 @@ function build_flight_interface(){
 	body.empty();
 	body.append('<button id = "home_button"> Home </button>');
 	let listDiv = $('<div id = "flightsDiv">Flights</div>');
+	console.log(sel_id);
+	$.ajax(root_url + '/flights?filter[airline_id]=' + sel_id,
+	       {
+		   type: 'GET',
+		   xhrFields: {withCredentials: true},
+		   success: (response) => {
+		   		let count = 0;
+			   response.forEach(function(object) { 
+			   	let fl_num = object.number;
+			   	let dept_id = object.departure_id;
+			   	let arr_id = object.arrival_id;
+			   	count++;
+			   listDiv.append('<button class="in_flight" id=' + fl_num + '>Flight Number: ' + fl_num + '</button>');
+			   });
+			  	body.append('<p>Number of Optional Flights: ' + count + '</p>');
+		   },
+		   error: () => {
+		       alert('error with airline ID');
+		   }
+	       });
+
+	
 	body.append(listDiv);
 }
 
 function build_analytics_interface()	{
+	// let head = $('head');
+	// head.empty();
+	// head.append('<title>' + sel_airline + ' Cash Flow Statistics</title>');
+	// head.append('<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>');
+	// head.append('<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>');
+	// head.append('<script src = "script.js"> </script>');
+	// head.append('<script src = "google.js"> </script>');
 	let body = $('body');
 	body.empty();
 	body.append('<button id = "home_button"> Home </button>');
@@ -30,37 +60,50 @@ function build_analytics_interface()	{
 	body.append(listDiv);
 }
 
+
+
+
+
 function build_sel_page()	{
 	let body = $('body');
 	body.empty();
+	body.append('<button id = "logout"> Logout </button>')
 	body.append('<div class = "chooseDiv"> Please Select an Airline to View / Modify Financial Projections</div>');
-	
 	let dropdown = $('<div class="dropdown"></div>');
 	dropdown.append('<button onclick="dropdown()" class="dropbtn">Airlines</button>');
 	let myDD = $('<div id="myDropdown" class="dropdown-content">');
 	myDD.append('<input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">');
 
-
-	$.ajax(root_url + 'airlines',
+	$.ajax(root_url + '/airlines',
 	       {
 		   type: 'GET',
 		   xhrFields: {withCredentials: true},
 		   dataType: 'json',
 		   success: (response) => {
-		       if (response.status) {
-			   console.log('holy fuck it worked');
-		       }
+			   response.forEach(function(object) {myDD.append('<p class = "al" id =' + object.id +'>' + object.name + '</p>')});
 		   },
 		   error: () => {
 		   		console.log('error');
 		       alert('error');
 		   }
 	       });
-
-
 	dropdown.append(myDD);
 	body.append(dropdown);
 }
+
+function build_login_page()	{
+	let body = $('body');
+	body.empty();
+	body.append('<h1>Cash Flow Simulator Login</h1>');
+	let li = $('<div id = "login"></div>');
+		li.append('User:');
+		li.append('<input type = "text" id = "login_user"><br>');
+		li.append('Password:');
+		li.append('<input type = "text" id = "login_pass"><br>');
+		li.append('<button id = "login_btn">Login</button>');
+	body.append(li);
+}
+
 
 function dropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
@@ -84,20 +127,62 @@ function filterFunction() {
 
 
 
-
-
-
 $(document).ready(() => {
+
+    $('body').on('click', '#login_btn', function()  {
+	
+	let user = $('#login_user').val();
+	let pass = $('#login_pass').val();
+
+	$.ajax(root_url + "/sessions",
+	       {
+		   type: 'POST',
+		   xhrFields: {withCredentials: true},
+		   dataType: 'json',
+		   data: {
+		   		"user": {
+		       		"username": user,
+		       		"password": pass
+		   				}
+			},	
+		   success: (response) => {
+			   build_sel_page();
+		   },
+		   error: () => {
+		       alert('Wrong Username or Password, try again');
+		   }
+	       });
+    });
+
+$('body').on('click', '#logout', function()	{
+	$.ajax(root_url + "/sessions",
+	       {
+		   type: 'DELETE',
+		   xhrFields: {withCredentials: true},
+		   success: (response) => {
+			   build_login_page();
+		   },
+		   error: () => {
+		       alert('Something is Wrong with Logout');
+		   }
+	       });
+    });
+
+
+
 
 $('body').on('click', '.al', function()	{
 	sel_airline = this.innerText;
+	sel_id = parseInt($(this).attr('id'));
 	console.log(sel_airline);
+	console.log(sel_id);
 	build_homepage();
 });
 
 
 $('body').on('click', '#choose_new_al', function()	{
 	sel_airline = '';
+	sel_id = 0;
 	build_sel_page();
 });
 
